8d53888567a3f2872f4164b1f0ddeaf0aeaab82a
NadaMaliki <nadamaliki2105@gmail.com>
2025-12-16 14:57:02 +0100

    ajout des fichiers d'environnement

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.yourdomain.com/graphql', // changer selon ton déploiement
+  authUrl: 'https://api.yourdomain.com/auth',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/graphql', // changer selon backend local
+  authUrl: 'http://localhost:3000/auth',
+};
